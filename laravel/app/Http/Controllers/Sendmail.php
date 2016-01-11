2957edc62762ae87927720b7bbda25cd7db53361
geniuspom <pakorn_r@outlook.com>
2016-01-11 18:47:32 +0700

    edit send email control

--- laravel/app/Http/Controllers/Sendmail.php
+++ laravel/app/Http/Controllers/Sendmail.php
@@ -6,10 +6,11 @@ use Mail;
 use Request;
 use App\Models\validateuser as validateuser;
 use App\Models\Member as Member;
+use App\Models\reset_pw as Reset;
 use Redirect;
 
 class sendmail extends Controller{
-    
+
     //gen code
     public function generateRandomString($length = 15) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -22,40 +23,91 @@ class sendmail extends Controller{
     }
 
     public function sendEmailReminder(){
-        
-        $validate = validateuser::validateforgot(Request::all());
-        
+
+
         if($validate->passes()){
-            
+
             $count = Member::where('email', '=', Request::input('email'))->count();
 
             if($count == 1){
-                
+
                 $forgot_code = sendmail::generateRandomString();
                 $sforgot_code = MD5($forgot_code);
-        
-                Mail::send('Member.mailforgot', array('code'=>$sforgot_code), function ($message) {
+
+                Mail::send('Member.mailvalidate', array('code'=>$sforgot_code), function ($message) {
 
                     $message->to(Request::input('email'))->subject('Your Reminder!');
-            
+
                 });
-                
+
                 return Redirect::to('/');
-                
+
             }else{
                 $msg = "Not found email!";
-                
+
                 return Redirect::to('forgot')
                     ->withInput(Request::except('password'))
                     ->withErrors($msg);
             }
-            
+
         }else{
             return Redirect::to('forgot')
                     ->withInput(Request::except('password'))
                     ->withErrors($validate->messages());
         }
-        
+
     }
-    
+
+    public function sendEmailForgot(){
+
+        $validate = validateuser::validateforgot(Request::all());
+
+        if($validate->passes()){
+
+            $count = Member::where('email', '=', Request::input('email'))->count();
+
+            if($count == 1){
+
+                $forgot_code = sendmail::generateRandomString();
+                $sforgot_code = MD5($forgot_code);
+
+                $reset = new Reset();
+                $reset->email = Request::input('email');
+                $reset->token = $sforgot_code;
+                $reset->save();
+
+                $profiles = Member::::where('email', '=', Request::input('email'))->get();
+
+                foreach ($profiles as $recode){
+                  $recode->name;
+                }
+
+                $name = $record[1];
+
+                $link = 'http://localhost/4oj/reset?token=' . $sforgot_code;
+
+                Mail::send('Member.mailforgot', array('code'=>$sforgot_code,'name'=>$name,'link'=>$link), function ($message) {
+
+                    $message->to(Request::input('email'))->subject('Your Password Reset Link!');
+
+                });
+
+                return Redirect::to('/');
+
+            }else{
+                $msg = "Not found email!";
+
+                return Redirect::to('forgot')
+                    ->withInput(Request::except('password'))
+                    ->withErrors($msg);
+            }
+
+        }else{
+            return Redirect::to('forgot')
+                    ->withInput(Request::except('password'))
+                    ->withErrors($validate->messages());
+        }
+
+    }
+
 }
