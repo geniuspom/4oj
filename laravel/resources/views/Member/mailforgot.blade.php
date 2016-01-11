dd6bf9d5ac32171dcaea2583c5b0bad45554b9c6
geniuspom <pakorn_r@outlook.com>
2016-01-11 16:00:28 +0700

    update mail template

--- laravel/resources/views/Member/mailforgot.blade.php
+++ laravel/resources/views/Member/mailforgot.blade.php
@@ -1,13 +1,32 @@
-<h2>
-    ยินดีต้อนรับเข้าร่วมเป็นสมาชิกของ 4oj
-</h2>
-<p>
-    ขอบคุณที่เข้าร่วมกับทางเว็บไซต์ กรุณาคลิ๊กลิงค์ด้านล่างเพื่อทำการยืนยันด้วยครับ
-</p>
-<a href="http://4oj-ojcon.rhcloud.com/activate/{{ $code }}" >http://4oj-ojcon.rhcloud.com/activate/{{ $code }}</a>
-
-<p>
-    Best Regard <br/>
-    Admin<br/>
-    4oj
-</p>
+<table class="ecxmainTable ecxdevWidth" style="font-size:inherit;line-height:inherit;text-align:center;border-spacing:0;border-collapse:collapse;padding:0px;border:0px;font-family: Helvetica Neue,Arial,sans-serif;" align="center" border="0" cellpadding="0" cellspacing="0" width="685px">
+         <tbody><tr style="width:685px;" class="ecxdevWidth">
+          <td class="ecxcenterColumn ecxcenterColumnSpacer" style="padding-right:36px;padding-left:40px;">
+            <table style="font-size:inherit;line-height:inherit;padding:0px;border:0px;">
+              <tbody><tr>
+                <td class="ecxletter_heading_logo" style="padding-top:8px;text-align:right;" align="right"><img src="" ></td>
+              </tr>
+               <tr>
+				   <td class="ecxcontent ecxspacerSalutation" style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:15px;text-align:left;">Dear ,
+				  </td>
+			   </tr>
+			   <tr>
+				   <td class="ecxcontent ecxspacer1" style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:34px;text-align:left;"><p>Click here to reset your password:</p>
+				     <p><a href="http://support.apple.com/kb/HT5661?viewlocale=en_US" style="color:rgb(0, 138, 205);text-decoration:none;" target="_blank">&gt;&gt;Link&lt;&lt;</a></p>
+				   </td>
+			   </tr>
+			   <tr>
+			   	   <td class="ecxcontent ecxspacer2" style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:29px;text-align:left;">
+						OJ Consulting Group</td>
+			   </tr>
+            </tbody></table>
+          </td>
+        </tr>
+	   <tr>
+		 <td class="footer ecxbackground ecxcenterColumn ecxdevWidth" id="ecxfooterGroup1" colspan="3" style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;width:685px;font-size:11px;line-height:14px;color:#888;text-align:center;background-repeat:no-repeat;background-position:center top;padding:19px 0 12px;" align="center" background="https://statici.icloud.com/emailimages/v4/common/footer_gradient_web.png">&nbsp;</td>
+	   </tr>
+	   <tr>
+		 <td class="footer ecxcenterColumn ecxcontent" colspan="3" style="padding-top:3px;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;width:685px;font-size:11px;line-height:14px;color:#888;text-align:center;" align="center">&nbsp;</td>
+	   </tr>
+	   <tr class="ecxfooterBottomPadding" style="height:50px;" height="50"><td style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;"></td></tr>
+      </tbody>
+    </table>
---
+++ laravel/resources/views/Member/mailvalidate.blade.php
@@ -0,0 +1,43 @@
+<table style="font-size:inherit;line-height:inherit;text-align:center;border-spacing:0;border-collapse:collapse;padding:0px;border:0px;font-family: Helvetica Neue,Arial,sans-serif;" align="center" border="0" cellpadding="0" cellspacing="0" width="685px">
+    <tbody><tr style="width:685px;" class="ecxdevWidth">
+          <td style="padding-right:36px;padding-left:40px;">
+            <table style="font-size:inherit;line-height:inherit;padding:0px;border:0px;">
+              <tbody><tr>
+                <td style="padding-top:8px;text-align:right;" align="right"><img src="" ></td>
+              </tr>
+               <tr>
+				   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:15px;text-align:left;">Welcome ,
+				  </td>
+			   </tr>
+			   <tr>
+				   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:15px;text-align:left;">
+					    Thank your for creating your OJ account!</td>
+			   </tr>
+			   <tr>
+				   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:15px;text-align:left;">
+					    Your account will be used to login to OJ websites.</td>
+			   </tr>
+			   <tr>
+				   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:15px;text-align:left;">&nbsp;</td>
+			   </tr>
+			   <tr>
+				   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:34px;text-align:left;"><p>Please click the confirmation link below to complete the validation process. Once you have successfully validated your account, you will be able to use all functions on OJ systems.</p>
+				     <p>Confirmation Link: <a href="http://4oj-ojcon.rhcloud.com/activate/{{ $code }}" style="color:rgb(0, 138, 205);text-decoration:none;" target="_blank">http://4oj-ojcon.rhcloud.com/activate/{{ $code }}</a></p>
+				   </td>
+			   </tr>
+			   <tr>
+			   	   <td style="word-break:break-word;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#333;font-size:15px;font-weight:300;line-height:20px;padding-bottom:29px;text-align:left;">
+						OJ Consulting Group</td>
+			   </tr>
+            </tbody></table>
+          </td>
+        </tr>
+	   <tr>
+		 <td colspan="3" style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;width:685px;font-size:11px;line-height:14px;color:#888;text-align:center;background-repeat:no-repeat;background-position:center top;padding:19px 0 12px;" align="center" background="https://statici.icloud.com/emailimages/v4/common/footer_gradient_web.png">&nbsp;</td>
+	   </tr>
+	   <tr>
+		 <td colspan="3" style="padding-top:3px;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;width:685px;font-size:11px;line-height:14px;color:#888;text-align:center;" align="center">&nbsp;</td>
+	   </tr>
+	   <tr style="height:50px;" height="50"><td style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;"></td></tr>
+      </tbody>
+</table>
