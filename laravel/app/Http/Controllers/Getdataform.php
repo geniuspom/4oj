09b1ebed21ddb4dd545674945c9a35511d3db971
geniuspom <pakorn_r@outlook.com>
2016-01-12 21:29:32 +0700

    Edit Getdateform Controller

--- laravel/app/Http/Controllers/Getdataform.php
+++ laravel/app/Http/Controllers/Getdataform.php
@@ -5,9 +5,9 @@ use App\Models\education as education;
 
 Class Getdataform extends Controller{
     public static function getbank($id){
-        
+
         $bank = bank::orderBy('id')->get();
-                                        
+
         echo "<select name='bank' id='bank' class='form-control'>";
             foreach ($bank as $recode){
                 echo "<option value='".$recode->id."'" ;
@@ -18,11 +18,11 @@ Class Getdataform extends Controller{
             }
         echo "</select>";
     }
-    
+
     public static function geteducation($id){
-        
+
         $education = education::orderBy('id')->get();
-                                 
+
         echo "<select name='education' id='education' class='form-control'>";
             foreach ($education as $recode){
                 echo "<option value='".$recode->id."'" ;
@@ -33,6 +33,5 @@ Class Getdataform extends Controller{
             }
         echo "</select>";
     }
-    
-}
 
+}
