e883686b09412350d5e9457337af2f23f07f6941
geniuspom <pakorn_r@outlook.com>
2016-01-12 21:28:42 +0700

    Update validation function
    Add validate when update user

--- laravel/app/Models/validateuser.php
+++ laravel/app/Models/validateuser.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 
 class validateuser extends Model {
-    
+
     public static function validate($input){
-    
+
     $rules = array(
         'name' => 'Required',
         'surname' => 'Required',
@@ -15,20 +15,32 @@ class validateuser extends Model {
         'password' => 'Required|AlphaNum|Between:6,16|Confirmed',
         'password_confirmation' => 'Required|AlphaNum|Between:6,16',
         'phone' => 'Required|Numeric',
-        'id_card' => 'Required|Numeric|digits:13|Unique:users'    
+        'id_card' => 'Required|Numeric|digits:13|Unique:users'
     );
-    
+
         return Validator::make($input, $rules);
     }
-    
+
     public static function validateforgot($input){
-        
+
         $rules = array(
             'email' => 'Required|Between:3,64|Email'
         );
-        
+
         return Validator::make($input, $rules);
-        
+
+    }
+
+    public static function validateupdateuser($input){
+
+    $rules = array(
+        'name' => 'Required',
+        'surname' => 'Required',
+        'nickname' => 'Required',
+        'phone' => 'Required|Numeric'
+    );
+
+        return Validator::make($input, $rules);
     }
 
 }
