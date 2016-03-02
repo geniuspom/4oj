cd86257128a790d7a7672e9c453f853315a87693
geniuspom <pakorn_r@outlook.com>
2016-03-03 00:23:34 +0700

    Edit autocompleted to realtime

--- public/js/autoCompleteVenue.js
+++ public/js/autoCompleteVenue.js
@@ -1,6 +1,6 @@
 //$(function() {
 
-$(function() {
+/*$(function() {
 
   var fullpath = window.location.href;
   var rootpath = window.location.origin;
@@ -34,4 +34,38 @@ $(function() {
       autoFocus:true
   });
 
+});*/
+
+$("#Venue_value").on("keypress", function () {
+  $("#Venue_value").autocomplete({
+      source: getdata(),
+      autoFocus:true
+  });
 });
+
+function getdata(){
+
+  var fullpath = window.location.href;
+  var rootpath = window.location.origin;
+  var splite_path = fullpath.split('/');
+  var subdomain = splite_path[3];
+  var url = rootpath+'/'+subdomain + '/get_room_autocomplete';
+
+    var availableTags = null;
+
+    $.ajaxSetup({
+       headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
+    });
+
+    $.ajax({
+      async: false,
+      global: false,
+      url: url,
+      dataType:'json',
+      success: function(response) {
+        availableTags = response;
+      }
+    });
+    return availableTags;
+
+}
